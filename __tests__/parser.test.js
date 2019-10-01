35783795a414c5bdc9959007a7a794ec6a2935fd
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-10-01 06:23:07 +0200

    reading html for tests

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,4 +1,10 @@
 const parser = require('../parser');
+const fs = require('fs');
+let html;
+
+beforeAll(() => {
+    html = fs.readFileSync('./test.html');
+})
 
 it('should give 4', () => {
     const result = parser.add(2, 2);
